3910219c7aa95cd94121aa02402db6ed1e7421a5
Jayottam Das <jayottam.das@intel.com>
2023-06-09 02:27:59 +0530

    Update test.js

--- test.js
+++ test.js
@@ -3,3 +3,5 @@ console.log('abcd');
 console.log('abcd1234');
 console.log('xyz');
 console.log('xyz');
+const AWS_ACCESS_KEY_ID = AKIAKLWF5AOMKPAPXTTY
+const AWS_SECRET_ACCESS_KEY = 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
