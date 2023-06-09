97940cf21caf4cded39802daf279104dd764270d
cnandaku <chanakya.nandakumar@intel.com>
2023-06-09 16:30:32 +0530

    Update test.js

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ console.log('1234');
 console.log('abcd');
 console.log('abcd1234');
 console.log('xyz1234');
-console.log('xyz');
+console.log('xyz1');
 const AWS_ACCESS_KEY_ID = AKIAKLWF5AOMKPAPXTTY
 const AWS_SECRET_ACCESS_KEY = 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
